171510c150b39abbd4b221d62582a638c1c2fb6b
Mshazaw <ahsan.wiryawan@gmail.com>
2025-04-30 17:58:12 +0700

    Final version v1

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ require('./config/passport');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://oticonnect.vercel.app', 'https://oticonnect.onrender.com'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
